2eee4724318224c971b7e21a792d7911f19d9f03
mnz <muneeskarakkad62@gmail.com>
2023-06-26 12:29:58 +0530

    intro page completed

--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -1,11 +1,27 @@
-import './intro.scss'
+import "./intro.scss";
 
 const Intro = () => {
   return (
-    <div className='intro' id='intro'>
-      Intro 
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" className="image" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Person Name</h1>
+          <h3>
+            Freelancer <span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="down" />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
