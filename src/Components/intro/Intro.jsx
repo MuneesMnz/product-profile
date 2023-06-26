1a742050f500bb11920d14420df5510cfa1b1a86
mnz <muneeskarakkad62@gmail.com>
2023-06-26 13:03:15 +0530

    text typing animation in intro completed

--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -1,6 +1,21 @@
+import { init } from "ityped";
 import "./intro.scss";
+import { useEffect } from "react";
+import { useRef } from "react";
 
 const Intro = () => {
+  const textRef = useRef();
+
+  useEffect(() => {
+
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,
+
+      strings: ["Developer", "Designer"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -13,7 +28,7 @@ const Intro = () => {
           <h2>Hi There, I'm</h2>
           <h1>Person Name</h1>
           <h3>
-            Freelancer <span></span>
+            Freelancer <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portfolio">
